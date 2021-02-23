295e8a7a82ef24fd0085204bafedf7df793b21ff
JorgeSerranoP <100405987@alumnos.uc3m.es>
2021-02-23 17:15:59 +0100

    Funcionalidad total

--- Micropractica1/src/app/app.component.ts
+++ Micropractica1/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   title = 'COMANDA';
-
+  public total:number = 0;
   public lineas:Array<[string,number]>=[];
   public cod_comand = 0;
   public comanda:Array<[string,number]>=[['Cerdo agridulce',5.5],['Arroz tres delicias',3.5],['Ternera con bambu y setas',7]];
@@ -16,6 +16,9 @@ export class AppComponent {
   public addLinea(i: number)
   {
 	  this.lineas.push(this.comanda[i]);
+    this.total += this.lineas[i][1];
+    console.log(this.comanda[i][1]);
+    console.log(this.total);
   }
   
   public delLinea(i: number)
